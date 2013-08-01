cad5bfa8db59a8a2232293f2183ec200cff1b18c
Manuel J. Garrido <manuel.garrido@gmail.com>
2013-08-01 09:16:34 +0200

    Update sol.sh

--- sol.sh
+++ sol.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-PID=$(ps ax -o "pid,comm" | grep $1$ | cut -d\   -f2)
+PSFILE=$(mktemp /tmp/solXXXX)
 
-echo PID: $PID
+ps x -o "pid,comm" | grep -i " $1$" > $PSFILE
 
-COUNT=$(echo $PID | wc -w)
-
-#echo Hay $COUNT instancias
-
-case "$COUNT" in
-0)
-	#echo hay que lanzar la app
+if [ $? != 0 ] # No hay proceso para la aplicación: se lanza.
+then
 	$1 &
+	rm -f $PSFILE
+	exit 0
+fi
+
+WFILE=$(mktemp /tmp/solwXXXX)
+
+wmctrl -pl | sort -bg --key=3 | join -1 1 -2 3 $PSFILE - > $WFILE
+
+case "$(cat $WFILE | wc -l)" in
+0) # Proceso sin ventana, no hacer nada
 	;;
-1)
-	#echo hay que enfocarla
-	wmctrl -ia $(wmctrl -lp | fgrep $PID | cut -f1 -d\ )
+
+1) # Una sola ventana: se activa.
+	wmctrl -ia $(cut -f3 -d\  $WFILE)
 	;;
-*)
-	#echo hay que sacar una lista
+
+*) # Más de una ventana: hay que elegir qué ventana mostrar.
+	V=$(awk '{print $3; $1=$2=$3=$4=$5=""; sub(/^  */,"", $0); print $0}' $WFILE |\
+     	    zenity --list --title "Elige ventana" --text=$1 \
+       		--column "Código" --column "Título" --hide-column=1)
+
+	[ -n "$V" ] && wmctrl -ia $V # Si se ha elegido una ventana se muestra.
 	;;
 esac
+
+rm -f $PSFILE $WFILE
